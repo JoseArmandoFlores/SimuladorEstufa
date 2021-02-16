644357344e868e1ae934f555d1f45dacbf965b1e
Estiven Padilla <estivenps26@gmail.com>
2021-02-15 22:03:52 -0400

    yamile vaqueame lo perro de lo pie

--- SimuladorEstufa/MainForm.Designer.cs
+++ SimuladorEstufa/MainForm.Designer.cs
@@ -28,21 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.TabControl1 = new System.Windows.Forms.TabControl();
+            this.tagPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.TabControl1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // TabControl1
+            // 
+            this.TabControl1.Controls.Add(this.tagPage1);
+            this.TabControl1.Controls.Add(this.tabPage2);
+            this.TabControl1.Font = new System.Drawing.Font("Georgia", 10.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TabControl1.Location = new System.Drawing.Point(12, 12);
+            this.TabControl1.Name = "TabControl1";
+            this.TabControl1.SelectedIndex = 0;
+            this.TabControl1.Size = new System.Drawing.Size(1120, 631);
+            this.TabControl1.TabIndex = 0;
+            // 
+            // tagPage1
+            // 
+            this.tagPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tagPage1.Location = new System.Drawing.Point(4, 34);
+            this.tagPage1.Name = "tagPage1";
+            this.tagPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tagPage1.Size = new System.Drawing.Size(1112, 603);
+            this.tagPage1.TabIndex = 0;
+            this.tagPage1.Text = "Estufa";
+            this.tagPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 30);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(1112, 597);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Alimentos";
+            this.tabPage2.UseVisualStyleBackColor = true;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 503);
+            this.ClientSize = new System.Drawing.Size(1144, 665);
+            this.Controls.Add(this.TabControl1);
             this.Name = "MainForm";
             this.Text = "Simulador Estufa";
             this.Load += new System.EventHandler(this.MainForm_Load);
+            this.TabControl1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.TabControl TabControl1;
+        private System.Windows.Forms.TabPage tagPage1;
+        private System.Windows.Forms.TabPage tabPage2;
     }
 }
 
