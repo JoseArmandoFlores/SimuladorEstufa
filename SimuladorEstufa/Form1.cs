0c1baca7e2d94b32652c2dfce7533bd45ab0c7c8
Jose Armando Flores Baldera <jose_flores3@ucne.edu.do>
2021-02-15 21:24:31 -0400

    Add project files.

---
+++ SimuladorEstufa/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimuladorEstufa
+{
+    public partial class MainForm : Form
+    {
+        public const double LenaSeca = 4.5;//Kilocalorias por gramos (Kcal/Gr) = 1.25Kcal/h
+        public const double CascaraSecaArroz = 3.8;
+        public const double HojaSeca = 4.5;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
